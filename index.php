fb6f06d3f63542f8e57dafdff9e0482edd883c50
Prudentay <prudentprogrammer@gmail.com>
2018-02-04 20:35:22 +0100

    Commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>..:: Havanacoin ::..</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap_3.css">
+    <link rel="stylesheet" type="text/css" href="css/havana_ui.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <link rel="icon" href="img/logo/logo.png">
+
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50" style="padding: 0">
+
+<nav class="navbar navbar-blue navbar-offcanvas navbar-inverse navbar-fixed-top" style="border-bottom: 1px solid #fff">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" style="margin-right: -20px">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="index.html">
+                <span style="float: right" class="siteName" style="margin-right: 20px;">
+                    &ensp;Havanacoin
+                </span>
+                <img src="img/logo/logo.png" width="35px" style="margin-top: -5px; margin-left: 15px">
+            </a>
+
+
+            <div class="visible-xs mobile-header">
+                <ul>
+                    <li style=" background: none;"><a href="register.html">Register</a></li>
+                    <li style="margin-right: 0;"><a href="login.html">Login</a></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class=""><a href="">Home</a></li>
+                <li><a href="">Our Vision</a></li>
+                <li><a href="">GitHub</a></li>
+                <li><a href="">Whitepaper</a></li>
+                <li><a href="">ICO</a></li>
+                <li><a href="">Our Programs</a></li>
+                <li><a href="">Register</a></li>
+                <li><a href="">Login</a></li>
+
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+
+
+
+
+<section class="content" style="min-height: 800px;margin-top: 0;">
+    <div class="first_row">
+
+        <div class="container">
+            <div class="col-sm-6">
+                <div class="welcome_note">
+                    <h2>Welcome to Havacoin</h2>
+                    <p>Blockchain's first to combine investment opportunity with incentivized decentralized community platform
+                    <br>
+                    (An investment platform payment ledger and blockchain bond reward community)</p>
+                </div>
+
+                <a href="" class="btn btn-primary" style="width: 130px;">Login</a>
+                <a href="" class="btn btn-danger" style="width: 130px;">Register</a>
+
+            </div>
+
+
+            <div class="col-sm-6">
+                <div class="" style="margin-top: 100px">
+                    <img src="img/laptop2.png"  class="animated bounce" style="margin-top: 30px" width="100%">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" style="min-height: 350px">
+        <div class="col-sm-4">
+            <h3 class="t-h-font-ubuntu">Built on Etherium </h3>
+            <p class="text-left p1">Built on Etherium's block chain technology, with the ability to leverage the power smart contract creation</p>
+        </div>
+
+        <div class="col-sm-2 hidden-xs"></div>
+
+
+        <div class="col-sm-6">
+
+            <img src="img/builtonethereum.png" width="80%">
+
+        </div>
+
+    </div>
+
+    <div class="" style="min-height: 350px;background: #292f5e;padding-top: 30px">
+
+        <div class="container">
+            <div class="col-sm-6">
+
+                <img src="img/vision.png" width="90%">
+
+            </div>
+
+            <div class="col-sm-6">
+                <div class="vision_page">
+                    <h4 class="text-left" style="margin-bottom: -30px">Our Vision</h4>
+
+                    <h6 class="t-h-font-roboto">Havanacoin is an hybrid peer to peer decentralized stable coin designed to create wealth</h6>
+
+                    <p class="t-h-font-roboto" style="margin-bottom: 2px">
+                        <b>Hybrid (Lending program & incentivized online blockchain community platform)</b>
+                    </p>
+
+                    <p class="t-h-font-roboto">
+                        Havanacoin is borne out of committment to create a community driven decentralized cryptocurrency that gives investors limitless opportunites to stake, invest, and in return earn a substantial interest on investment.
+                    </p>
+
+                    <p class="t-h-font-roboto">Individual will be rewarded for their post contents, comments and likes via our incentivized online blockchain community platform.</p>
+
+                    <p class="t-h-font-roboto">
+                        This will serve as a channel in which business,industries and society can achieve prosperity competitiveness and social freedom through blockchain and what individuals need to know to be able to navigate a cyber economy either in trading or investing in long term oriented projects.
+                    </p>
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <div class="row_3">
+        <div class="container" style="padding-top: 40px">
+
+            <div class="col-sm-6"></div>
+
+            <div class="col-sm-6 right" style="padding-top: 50px">
+
+                <h5 class="t-h-font-ubuntu">Most Secure Platform</h5>
+                <p class="p1">
+                    Advanced distributed system architecture built to protect against DDoS and other potential threats. More than 98% of digital assets stored in multi-signature,cold wallets. Security protocols are fully aligned and compliant with industry best practices. 4 years of safe,stable infrastructure operations, giving customers 100% confidence.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="" style="min-height: 200px;background: #123c79;padding-top: 20px">
+
+        <div class="container">
+            <div class="col-sm-6" style="color: #fff">
+                <h4 style="color: #fff" class="t-h-font-ubuntu">A Truly Global Exchange</h4>
+                <p class="p1">
+                    Empowering millions of users, across 130+ countries. Over $1 Billion in assets. 24/7/365 customer access and support. A trusted world-class operation designed specifically for knowledgeable crypto-investors.
+                </p>
+            </div>
+
+            <div class="col-sm-1"></div>
+
+            <div class="col-sm-5">
+
+                <img src="img/global.png" width="90%">
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <div id="stats">
+        <div class="container">
+            <div class="wrapper">
+                <div id="currentsupply" style="margin-bottom: -100px;">
+                    <span>Current Havana Supply</span>
+                    <div id="supply">30,939,201</div>
+                </div>
+                <div id="batchsupply">
+                    <div id="minsupply">
+                        Minimum Final Supply<br>
+                        (Min Batch Size)<br>
+                        <span id="min-final-batch" class="amount">54,917,017</span>
+                        <img src="img/minbatch.png">
+                    </div>
+                    <div id="maxsupply">
+                        Maximum Final Supply<br>
+                        (Max Batch Size)<br>
+                        <span id="max-final-batch" class="amount">112,201,332</span>
+                        <img src="img/maxbatch.png">
+                    </div>
+                </div>
+                <div class="statsrow">
+                    <li>
+                        Current Elix Price (BTC)
+                        <div id="elixpricebtc" class="stat">0.00004384</div>
+                    </li>
+                    <li>
+                        Average Batches Used
+                        <div id="average-batches" class="stat">6.08</div>
+                    </li>
+                    <li>
+                        Total Batches Remaining
+                        <div id="total-batches" class="stat">16,125</div>
+                    </li>
+                    <li>
+                        Current Batch Size
+                        <div id="batchsize" class="stat" title="1455.9468464611873">1455.95</div>
+                    </li>
+                    <li>
+                        Genesis Pairs Total
+                        <div class="stat">4,113</div>
+                    </li>
+                    <li>
+                        Percentage of Batches
+                        <div id="batches-percentage" class="stat">60.8%</div>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+</body>
+</html>
